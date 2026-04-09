158d50aa0a4f75353ad7f8e79eeeb28f7a8a50d2
Oscar Torres <166583974+O-Torress@users.noreply.github.com>
2026-04-09 18:04:18 -0400

    Refactorizar las clases de personajes y mejorar los métodos de ataque para una mejor funcionalidad.

--- Juego.js
+++ Juego.js
@@ -1,4 +1,4 @@
-class Personaje {
+class Personaje { //Clase Padre
   constructor(nombre, vida, dano, defensa, velocidad) {
     this.nombre = nombre;
     this.vida = vida;
@@ -6,81 +6,77 @@ class Personaje {
     this.defensa = defensa;
     this.velocidad = velocidad;
   }
-
+//Metdos
   atacar() {
     console.log(`${this.nombre} esta atacando con los puños`);
   }
 
   saludar() {
-    console.log(`Hola soy ${this.nombre} de la ${this.constructor.name}`);
+    console.log(`Hola soy el ${this.constructor.name} ${this.nombre} `);
   }
 }
-
-class Guerrero extends Personaje {
+//Guerrero
+class Guerrero extends Personaje {  
   constructor(nombre, vida, dano, defensa, velocidad, array_de_armas) {
     super(nombre, vida, dano, defensa, velocidad);
     this.array_de_armas = array_de_armas;
   }
 
-  atacar_con_arma() {
-    console.log(
-      `El guerrero ${this.nombre} esta atacando con el arma ${this.array_de_armas}`,
-    );
+  atacar_con_arma(objetivo) {
+    let armaAleatoria = this.array_de_armas [Math.floor(Math.random()*3)];
+    
+    if (armaAleatoria === "espada" || armaAleatoria === "lanza" ) {
+      console.log(
+        `El guerrero ${this.nombre} esta atacando a ${objetivo.nombre} con la ${armaAleatoria}`);
+    }else {
+      console.log(
+      `El guerrero ${this.nombre} esta atacando a ${objetivo.nombre} con el ${armaAleatoria}`);
+    }
+
+
+    
   }
 }
-
+//Mago
 class Mago extends Personaje {
   constructor(nombre, vida, dano, defensa, velocidad, array_de_hechizos) {
     super(nombre, vida, dano, defensa, velocidad);
     this.array_de_hechizos = array_de_hechizos;
   }
 
-  atacar_con_hechizo() {
+  atacar_con_hechizo(objetivo) {
+    this.array_de_hechizos = this.array_de_hechizos [Math.floor(Math.random()*3)];
     console.log(
-      `El Mago ${this.nombre} esta atacando con el arma ${this.array_de_hechizos}`,
-    );
+      `El Mago ${this.nombre} esta atacando a ${objetivo.nombre} con el hechizo de ${this.array_de_hechizos}`);
   }
 }
-
+//Arquero
 class Arquero extends Personaje {
   constructor(nombre, vida, dano, defensa, velocidad, array_de_flechas) {
     super(nombre, vida, dano, defensa, velocidad);
     this.array_de_flechas = array_de_flechas;
   }
 
-  disparar_con_flechas() {
+  disparar_con_flechas(objetivo) {
+    this.array_de_flechas = this.array_de_flechas [Math.floor(Math.random()*3)];
     console.log(
-      `El Arquero ${this.nombre} esta atacando con la flecha ${this.array_de_flechas}`,
-    );
+      `El Arquero ${this.nombre} esta atacando a ${objetivo.nombre} con las flechas echas de ${this.array_de_flechas}`);
   }
 }
 
-let personaje1 = new Guerrero("Floqui", "100", "50", "50", "60", [
-  "espada",
-  "cuchillo",
-  "lanza",
-]);
-let personaje2 = new Guerrero("Asquelad", "100", "45", "35", "80", [
-  "daga",
-  "mazo",
-  "hacha",
-]);
-let personaje3 = new Mago("Mandrake", "150", "60", "35", "80", [
-  "veneno",
-  "fuego",
-  "hielo",
-]);
-let personaje4 = new Mago("Gandalf", "160", "45", "35", "80", [
-  "Avada Kadabra",
-  "imperio",
-  "crucio",
-]);
-let personaje5 = new Arquero("Robin", "100", "65", "40", "100", [
-  "Piedra",
-  "cobre",
-  "metal",
-]);
+let personaje1 = new Guerrero("Floqui", 100, 50, 50, 10,   [ "espada", "cuchillo", "lanza"]);
+let personaje2 = new Guerrero("Asquelad", 100, 45, 35, 10, [ "daga", "mazo", "hacha"]);
+let personaje3 = new Mago("Mandrake", 150, 60, 35, 6,      [ "veneno", "fuego", "hielo"]);
+let personaje4 = new Mago("Gandalf", 160, 45, 35, 5,       [ "Avada Kadabra", "imperio", "crucio"]);
+let personaje5 = new Arquero("Robin", 100, 65, 40, 8,      [ "Piedra", "cobre", "metal"]);
 
-personaje1.saludar();
-personaje2.saludar();
-personaje4.saludar();
+const Listapersonajes = [personaje1,personaje2,personaje3,personaje4,personaje5];
+
+Listapersonajes.forEach(p => p.saludar());
+
+personaje1.atacar_con_arma(personaje2);
+/*
+while (Listapersonajes.length > 1) {
+  console.log("Ronda 1");
+}
+*/
