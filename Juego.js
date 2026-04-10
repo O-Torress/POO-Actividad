bbfd7fdad8eadfa9d0b2508cd5883386e4646237
Oscar Torres <166583974+O-Torress@users.noreply.github.com>
2026-04-10 16:45:31 -0400

    Mejorar la lógica de ataque en las clases de personajes para incluir daño basado en armas y hechizos.

--- Juego.js
+++ Juego.js
@@ -7,7 +7,7 @@ class Personaje { //Clase Padre
     this.velocidad = velocidad;
   }
 //Metdos
-  atacar() {
+  atacar(objetivo) {
     console.log(`${this.nombre} esta atacando con los puños`);
   }
 
@@ -23,18 +23,20 @@ class Guerrero extends Personaje {
   }
 
   atacar_con_arma(objetivo) {
-    let armaAleatoria = this.array_de_armas [Math.floor(Math.random()*3)];
+    this.array_de_armas = this.array_de_armas [Math.floor(Math.random()*3)]; 
+    let danoTotal = this.dano + this.array_de_armas.dano;
+    console.log(
+      `El guerrero ${this.nombre} esta atacando a ${objetivo.nombre} con ${this.array_de_armas.nombre} y le hizo ${danoTotal} puntos de daño.`);
     
-    if (armaAleatoria === "espada" || armaAleatoria === "lanza" ) {
-      console.log(
-        `El guerrero ${this.nombre} esta atacando a ${objetivo.nombre} con la ${armaAleatoria}`);
-    }else {
-      console.log(
-      `El guerrero ${this.nombre} esta atacando a ${objetivo.nombre} con el ${armaAleatoria}`);
+    let danoa = danoTotal - objetivo.defensa > 0 ? danoTotal - objetivo.defensa : 0;
+
+    objetivo.vida -= danoa;
+    
+    console.log(`${objetivo.nombre} recibe ${danoa} de daño, su vida ahora es ${objetivo.vida}`);
+
+    if (objetivo.vida <= 0) {
+      console.log(`${objetivo.nombre} ha sido derrotado por ${this.nombre}`);
     }
-
-
-    
   }
 }
 //Mago
@@ -46,8 +48,9 @@ class Mago extends Personaje {
 
   atacar_con_hechizo(objetivo) {
     this.array_de_hechizos = this.array_de_hechizos [Math.floor(Math.random()*3)];
+    let danoTotal = this.dano + this.array_de_hechizos.dano;
     console.log(
-      `El Mago ${this.nombre} esta atacando a ${objetivo.nombre} con el hechizo de ${this.array_de_hechizos}`);
+      `El Mago ${this.nombre} esta atacando a ${objetivo.nombre} con el hechizo de ${this.array_de_hechizos.nombre} y le hizo ${danoTotal} puntos de daño`);
   }
 }
 //Arquero
@@ -59,24 +62,24 @@ class Arquero extends Personaje {
 
   disparar_con_flechas(objetivo) {
     this.array_de_flechas = this.array_de_flechas [Math.floor(Math.random()*3)];
+    let danoTotal = this.dano + this.array_de_flechas.dano;
     console.log(
-      `El Arquero ${this.nombre} esta atacando a ${objetivo.nombre} con las flechas echas de ${this.array_de_flechas}`);
+      `El Arquero ${this.nombre} esta atacando a ${objetivo.nombre} con las flechas echas de ${this.array_de_flechas.nombre} y le hizo ${danoTotal} puntos de daño`);
   }
 }
 
-let personaje1 = new Guerrero("Floqui", 100, 50, 50, 10,   [ "espada", "cuchillo", "lanza"]);
-let personaje2 = new Guerrero("Asquelad", 100, 45, 35, 10, [ "daga", "mazo", "hacha"]);
-let personaje3 = new Mago("Mandrake", 150, 60, 35, 6,      [ "veneno", "fuego", "hielo"]);
-let personaje4 = new Mago("Gandalf", 160, 45, 35, 5,       [ "Avada Kadabra", "imperio", "crucio"]);
-let personaje5 = new Arquero("Robin", 100, 65, 40, 8,      [ "Piedra", "cobre", "metal"]);
+let personaje1 = new Guerrero("Floqui", 100, 50, 50, 10,   
+  [ {nombre : "espada" , dano : 25 }, {nombre : "cuchilo" , dano : 20 }, {nombre : "lanza" , dano : 15 }]);
+let personaje2 = new Guerrero("Asquelad", 100, 45, 35, 10, 
+  [ {nombre : "daga" , dano : 10 },{nombre : "mazo" , dano : 20 }, {nombre : "hacha" , dano : 25 }]);
+let personaje3 = new Mago("Mandrake", 150, 60, 35, 6,      
+  [ {nombre : "veneno" , dano : 35 }, {nombre : "fuego" , dano : 30 }, {nombre : "hielo" , dano : 20 }]);
+let personaje4 = new Mago("Gandalf", 160, 45, 35, 5,       
+  [ {nombre : "Avada Kadabra", dano : 25}, {nombre : "imperio", dano : 30}, {nombre : "crucio", dano : 20}]);
+let personaje5 = new Arquero("Robin", 100, 65, 40, 8,      
+  [ {nombre : "Piedra" , dano : 25 }, {nombre : "cobre" , dano : 30 }, {nombre : "hierro" , dano : 35 }]);
 
-const Listapersonajes = [personaje1,personaje2,personaje3,personaje4,personaje5];
+const listaPersonajes = [personaje1,personaje2,personaje3,personaje4,personaje5];
 
-Listapersonajes.forEach(p => p.saludar());
-
+personaje5.disparar_con_flechas(personaje1);
 personaje1.atacar_con_arma(personaje2);
-/*
-while (Listapersonajes.length > 1) {
-  console.log("Ronda 1");
-}
-*/
