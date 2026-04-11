404208fbf3a949d819822d8b265979a4af4c7cf0
Oscar Torres <166583974+O-Torress@users.noreply.github.com>
2026-04-11 19:09:00 -0400

    Mejorar la lógica de ataque y la presentación de mensajes en las clases Guerrero, Mago y Arquero, implementando daño aleatorio y ajustando la selección de objetivos en la batalla.

--- Juego.js
+++ Juego.js
@@ -12,7 +12,7 @@ class Personaje { //Clase Padre
   }
 
   saludar() {
-    console.log(`Hola soy el ${this.constructor.name} ${this.nombre} `);
+    console.log(`Hola, soy el ${this.constructor.name} ${this.nombre} `);
   }
 }
 //Guerrero
@@ -24,7 +24,8 @@ class Guerrero extends Personaje {
 
   atacar_con_arma(objetivo) {
     const armaElegida = this.array_de_armas[Math.floor(Math.random() * this.array_de_armas.length)];
-    let danoTotal = this.dano + armaElegida.dano;
+    let danoTotal = Math.floor((Math.random() * armaElegida.dano + 1) + this.dano);
+    // this.dano + armaElegida.dano;
     console.log(
       `El guerrero ${this.nombre} esta atacando a ${objetivo.nombre} con ${armaElegida.nombre} y le hizo ${danoTotal} puntos de daño.`);
     
@@ -48,7 +49,7 @@ class Mago extends Personaje {
 
   atacar_con_hechizo(objetivo) {
     const armaElegida = this.array_de_hechizos[Math.floor(Math.random() * this.array_de_hechizos.length)];
-    let danoTotal = this.dano + armaElegida.dano;
+    let danoTotal = Math.floor((Math.random() * armaElegida.dano + 1) + this.dano);
     console.log(
       `El Mago ${this.nombre} esta atacando a ${objetivo.nombre} con el hechizo de ${armaElegida.nombre} y le hizo ${danoTotal} puntos de daño`);
 
@@ -72,7 +73,7 @@ class Arquero extends Personaje {
 
   disparar_con_flechas(objetivo) {
     const armaElegida = this.array_de_flechas[Math.floor(Math.random() * this.array_de_flechas.length)];
-    let danoTotal = this.dano + armaElegida.dano;
+    let danoTotal = Math.floor((Math.random() * armaElegida.dano + 1) + this.dano);
     console.log(
       `El Arquero ${this.nombre} esta atacando a ${objetivo.nombre} con las flechas echas de ${armaElegida.nombre} y le hizo ${danoTotal} puntos de daño`);
     
@@ -87,35 +88,35 @@ class Arquero extends Personaje {
     }
   }
 }
-
-let personaje1 = new Guerrero("Floqui", 100, 50, 50, 10,   
+// nombre, vida, dano, defensa, velocidad, [].
+let p1 = new Guerrero("Floqui", 100, 50, 50, 10,   
   [ 
-    {nombre : "espada" , dano : 25 }, 
-    {nombre : "cuchilo" , dano : 20 }, 
-    {nombre : "lanza" , dano : 15 }
+    {nombre : "espada" , dano : 100 },
+    {nombre : "cuchilo" , dano : 100 }, 
+    {nombre : "lanza" , dano : 100 }
   ]);
-let personaje2 = new Guerrero("Asquelad", 100, 45, 35, 10, 
+let p2 = new Guerrero("Asquelad", 100, 45, 50, 10, 
   [ 
     {nombre : "daga" , dano : 10 },
     {nombre : "mazo" , dano : 20 }, 
     {nombre : "hacha" , dano : 25 }
 
   ]);
-let personaje3 = new Mago("Mandrake", 110, 60, 35, 6,      
+let p3 = new Mago("Mandrake", 110, 60, 35, 6,      
   [ 
     {nombre : "veneno" , dano : 35 }, 
     {nombre : "fuego" , dano : 30 }, 
     {nombre : "hielo" , dano : 20 }
 
   ]);
-let personaje4 = new Mago("Gandalf", 110, 45, 35, 5,       
+let p4 = new Mago("Gandalf", 110, 45, 35, 5,       
   [ 
     {nombre : "Avada Kadabra", dano : 25}, 
     {nombre : "imperio", dano : 30}, 
     {nombre : "crucio", dano : 20}
 
   ]);
-let personaje5 = new Arquero("Robin", 100, 65, 40, 8,      
+let p5 = new Arquero("Robin", 100, 65, 40, 8,      
   [ 
     {nombre : "Piedra" , dano : 25 }, 
     {nombre : "cobre" , dano : 30 }, 
@@ -123,4 +124,59 @@ let personaje5 = new Arquero("Robin", 100, 65, 40, 8,
 
   ]);
 
-const listaPersonajes = [personaje1,personaje2,personaje3,personaje4,personaje5];
+let listaP = [p1,p2,p3,p4,p5];
+
+
+function elegirObjetivo(atacante, listaP) {
+  let vivos = listaP.filter(p => p !== atacante && p.vida > 0);
+  return vivos[Math.floor(Math.random() * vivos.length)];
+}
+
+
+console.log("\n--- INICIA LA BATALLA ---\n");
+listaP.forEach(p => p.saludar());
+
+let ronda = 1;
+
+
+while (listaP.filter(p => p.vida > 0).length > 1) {
+  console.log(`\n========= RONDA ${ronda} =========`);
+
+  
+  listaP.forEach(p => {
+    p.turno = Math.floor(Math.random() * p.velocidad) + 1;
+  });
+
+  listaP.sort((a, b) => b.turno - a.turno);
+
+  for (let atacante of listaP) {
+    if (atacante.vida <= 0) continue;
+
+    
+
+    let objetivo = elegirObjetivo(atacante, listaP);
+    if (!objetivo) continue;
+
+    let prob = Math.random();
+
+    if (prob < 0.33) {
+      atacante.atacar(objetivo);
+    } else {
+      if (atacante instanceof Guerrero) {
+        atacante.atacar_con_arma(objetivo);
+      } else if (atacante instanceof Mago) {
+        atacante.atacar_con_hechizo(objetivo);
+      } else if (atacante instanceof Arquero) {
+        atacante.disparar_con_flechas(objetivo);
+      }
+    }
+  }
+
+  
+  listaP = listaP.filter(p => p.vida > 0);
+
+  ronda++;
+}
+
+
+console.log(`\n EL GANADOR ES El ${listaP[0].constructor.name} ${listaP[0].nombre} `);
